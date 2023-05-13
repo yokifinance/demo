3b808d0747875c53dcaaeeb50e508adabff6ffe7
Stepan Bubnov <s.bubnov@s7.ru>
2023-05-13 06:55:17 +0300

    TRELLO-180: added new env values, added deployed contracts address

--- scripts/deploy_assets_whitelist.py
+++ scripts/deploy_assets_whitelist.py
@@ -4,13 +4,13 @@ from scripts.helpful_scripts import get_account
 
 core_assets_to_spend = [
     "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1", # DAI
-    "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8", # USDC
+    "0x2058A9D7613eEE744279e3856Ef0eAda5FCbaA7e", # USDC # Mubmai
     "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9" # USDT
 ]
 
 core_assets_to_buy = [
     "0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0",  # UNI
-    "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f", # WBTC
+    "0x326C977E6efc84E512bB9C30f76E30c160eD06FB", # CHAIN LINK # Mumbai
     "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1" # WETH
 ]
 
