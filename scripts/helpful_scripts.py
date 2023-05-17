6cc8fbdaeb4cf99a01f69da20a9009f7cd00d7d8
Stepan Bubnov <s.bubnov@s7.ru>
2023-05-17 09:18:08 +0300

    TRELLO-180: function refactored for using other wallet

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -4,9 +4,9 @@ FORKED_LOCAL_ENVIRENMENTS = ['mainnet-fork', 'mainnet-fork-dev', 'mumbai-fork-de
 LOCAL_BLOCKCHAIN_ENVIRENMENTS = ['development', 'ganache-local']
 
 
-def get_account():
+def get_account(account_label='main'):
     if(network.show_active() in LOCAL_BLOCKCHAIN_ENVIRENMENTS
         or network.show_active() in FORKED_LOCAL_ENVIRENMENTS
     ):
         return accounts[0]
-    return accounts.add(config['wallets']['from_key'])
+    return accounts.add(config['wallets'][account_label]['from_key'])
