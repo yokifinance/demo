0e465fc36f915a3a0a62d04352e3148f9805a0e6
Stepan Bubnov <s.bubnov@s7.ru>
2023-05-23 10:20:53 +0300

    TRELLO-180: script for execute DCA updated

--- scripts/execute_dca_strategy.py
+++ scripts/execute_dca_strategy.py
@@ -1,12 +1,14 @@
-import pytest
-from brownie import accounts, interface, web3
+from brownie import interface
+from eth_abi import encode
 
 from scripts.helpful_scripts import get_account
 from scripts.price_convertor import get_amount_wei
 
+
 def _get_encode_address_list(address_list: list):
-    encoded_path = web3.solidityKeccak(['address[]'], [address_list]).hex()
+    encoded_path = encode(['(address,address)'], [address_list])
     return encoded_path
+
 
 def execute_strategy(strategy_address: str, path: list):
     worker = get_account(account_label='worker')
@@ -14,20 +16,35 @@ def execute_strategy(strategy_address: str, path: list):
     
     encoded_path = _get_encode_address_list(path)
 
-    params_for_execute = (
-        encoded_path,
+    multi_params_for_execute = (
+        path,
         main_account,
         60 * 2, # 2 minutes Deadline
-        get_amount_wei(2, 6), # amountIn 2$
+        get_amount_wei(1, 6), # amountIn 2$
         get_amount_wei(1, 18), # amountOutMinimum
+    )
+
+    single_params_for_execute = (
+        path[0],
+        path[1],
+        3000, # pool Fee 0.3%
+        main_account,
+        60 * 2, # 2 minutes Deadline
+        1000000, # get_amount_wei(1, 6), # amountIn 1$
+        0, # amountOutMinimum
+        0 # sqrtPriceLimitX96
     )
     
     position_index: int = 0
     startegy = interface.IDCA(strategy_address)
-    tx = startegy.executeMultihopPurchase(position_index, params_for_execute, {'from': worker, 'gas_limit': 700000, 'allow_revert': True})
+
+
+    # tx = startegy.executeMultihopPurchase(position_index, params_for_execute, {'from': worker, 'gas_limit': 700000, 'allow_revert': True})
+    tx = startegy.executeSinglePurchase(position_index, single_params_for_execute, {'from': worker, 'gas_limit': 700000, 'allow_revert': True})
     tx.wait(1)
 
+
 def main():
-    path: list = ['0xc2132D05D31c914a87C6611C10748AEb04B58e8F', '0xb33EaAd8d922B1083446DC23f610c2567fB5180f']
-    deployed_strategy_address: str = '0xFeB3fFdb4628A80607FFb2249D6A42987d9Fd554'
+    path: list = ('0xc2132D05D31c914a87C6611C10748AEb04B58e8F', '0x9c2C5fd7b07E95EE044DDeba0E97a665F142394f')
+    deployed_strategy_address: str = '0xE108c1F9b0546dF81D6Ea598d0404BD48791EED6' #0x3A6e8a6Bfe6AB42d9E356b22704eb1029c4b6DF9'
     execute_strategy(deployed_strategy_address, path)
