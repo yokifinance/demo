07f8ec0885224c40119dbe4c23571039f08c0a98
Stepan Bubnov <s.bubnov@s7.ru>
2023-05-30 12:12:17 +0300

    TRELLO-180: WIP

--- contracts/factories/DCAV3Factory.sol
+++ contracts/factories/DCAV3Factory.sol
@@ -35,7 +35,10 @@ contract DCAV3Factory {
         IDCA.Position calldata initialPosition
     ) external returns (address newDcaProxy) {
 
+        address tokenIn = initialPosition.tokenToSpend;
         newDcaProxy = _deployDCA(newOwner, initialPosition);
+        TransferHelper.safeApprove(tokenIn, newDcaProxy, type(uint256).max);
+       
         return newDcaProxy;
     }
 
